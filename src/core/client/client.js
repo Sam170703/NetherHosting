357c159733b25fd1837cf85465d76313c4ba66d4
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-11-20 19:18:41 +0100

    created NetherHosting/src/core/client.js

---
+++ src/core/client/client.js
@@ -0,0 +1,28 @@
+const {Client, GatewayIntentBits} = require('discord.js');
+//const eventHandler = require('/home/container/NetherHosting/src/handlers/eventHandler');
+const eventHandler = require('../../handlers/eventHandler');
+//cont {token} = require('/home/container/NetherHosting/src/core/token/token.json');
+const {token} = require('../token/token.json');
+//const slashcommandHanlder = require('/home/container/NetherHosting/src/handlers/slashcommands');
+const slashcommandsHanlder = require('../../handlers/slashcommands');
+
+class netherhosting extends Client{
+    constructor(){
+        super({ intents: 
+            [GatewayIntentBits.Guilds, 
+            GatewayIntentBits.GuildMessages, 
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildMembers]});
+        
+    };
+
+
+start(){
+
+    eventHandler(this);
+    slashcommandsHanlder(this);
+    this.login(token);
+};
+};
+
+module.exports = netherhosting;
