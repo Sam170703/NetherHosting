39aaf129fa3f4eefbc1c711bc8c1c65ab9cb9144
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-11-20 19:26:43 +0100

    created NetherHosting/src/handlers/eventHandler.js

---
+++ src/handlers/eventHandler.js
@@ -0,0 +1,20 @@
+const {readdirSync} = require('fs');
+const path = require('path');
+
+async function eventHandler(netherhosting){
+
+    const dirPath = path.resolve(__dirname, '../events');
+    const Files = readdirSync(dirPath)
+    .filter((file) => file.endsWith('js'))
+    .map(async(file) => {
+        const event = require(`../events/${file}`);
+
+        if (event.once) {
+            netherhosting.once(event.name, (...args) => event.execute(netherhosting, ...args));
+        } else {
+            netherhosting.on(event.name, (...args) => event.execute(netherhosting, ...args));
+        }
+    });
+};
+
+module.exports = eventHandler;
