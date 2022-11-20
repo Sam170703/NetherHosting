0442c283ed0fb52a90abf7776b04d2d9672d9ee5
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-11-20 22:39:23 +0100

    changed path to NetherHosting Panel

--- src/core/client/client.js
+++ src/core/client/client.js
@@ -1,10 +1,10 @@
 const {Client, GatewayIntentBits} = require('discord.js');
-//const eventHandler = require('/home/container/NetherHosting/src/handlers/eventHandler');
-const eventHandler = require('../../handlers/eventHandler');
-//cont {token} = require('/home/container/NetherHosting/src/core/token/token.json');
-const {token} = require('../token/token.json');
-//const slashcommandHanlder = require('/home/container/NetherHosting/src/handlers/slashcommands');
-const slashcommandsHanlder = require('../../handlers/slashcommands');
+const eventHandler = require('/home/container/NetherHosting/src/handlers/eventHandler');
+//const eventHandler = require('../../handlers/eventHandler');
+const {token} = require('/home/container/NetherHosting/src/core/token/token.json');
+//const {token} = require('../token/token.json');
+const slashcommandHanlder = require('/home/container/NetherHosting/src/handlers/slashcommands');
+//const slashcommandsHanlder = require('../../handlers/slashcommands');
 
 class netherhosting extends Client{
     constructor(){
--- src/handlers/slashcommands.js
+++ src/handlers/slashcommands.js
@@ -6,22 +6,22 @@ async function slashcommandHandler(netherhosting) {
   let slash = []
 
   netherhosting.commands = new Collection();
-    //const dirPath = path.resolve(__dirname, '/home/container/NetherHosting/src/slashcommands');
-    const dirPath = path.resolve(__dirname, '../slashcommands');
+    const dirPath = path.resolve(__dirname, '/home/container/NetherHosting/src/slashcommands');
+    //const dirPath = path.resolve(__dirname, '../slashcommands');
     const commandFolders = readdirSync(dirPath);
 
     for (const folder of commandFolders) {
-        //const dirPath2 = path.resolve(__dirname, `/home/container/NetherHosting/src/slashcommands/${folder}`);
-      const dirPath2 = path.resolve(__dirname, `../slashcommands/${folder}`);
+        const dirPath2 = path.resolve(__dirname, `/home/container/NetherHosting/src/slashcommands/${folder}`);
+      //const dirPath2 = path.resolve(__dirname, `../slashcommands/${folder}`);
       const subfolders = readdirSync(dirPath2)
       for (const subfolder of subfolders) {
-        //const dirPath3 = path.resolve(__dirname, `/home/container/NetherHosting/src/slashcommands/${folder}/${subfolder}`);
-        const dirPath3 = path.resolve(__dirname, `../slashcommands/${folder}/${subfolder}`);
+        const dirPath3 = path.resolve(__dirname, `/home/container/NetherHosting/src/slashcommands/${folder}/${subfolder}`);
+        //const dirPath3 = path.resolve(__dirname, `../slashcommands/${folder}/${subfolder}`);
          const commandFiles = readdirSync(dirPath3)
         .filter((file) => file.endsWith(".js"));
         for (const file of commandFiles) {
-            //const command = require(`/home/container/NetherHosting/src/slashcommands/${folder}/${subfolder}/${file}`);
-          const command = require(`../slashcommands/${folder}/${subfolder}/${file}`);
+            const command = require(`/home/container/NetherHosting/src/slashcommands/${folder}/${subfolder}/${file}`);
+          //const command = require(`../slashcommands/${folder}/${subfolder}/${file}`);
   
            if (command.data.toJSON().name){
               netherhosting.commands.set(command.data.toJSON().name, { 
