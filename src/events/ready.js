2014a5384b70bd1660d61cc7bde8623db5651686
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-12-23 15:07:49 +0100

    updated ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,7 +6,7 @@ module.exports = {
     execute: async(netherhosting) => {
     
         var estado = "idle";
-        netherhosting.user.setActivity({name: `a SamuelVM#5390`, type: ActivityType.Listening});
+        netherhosting.user.setActivity({name: `a SamuelVM`, type: ActivityType.Listening});
         //Fuera de servicio temporalmente
         netherhosting.user.setStatus(`${estado}`);
         console.log(`Bot: ${netherhosting.user.username}\nEstado: ${estado}`);
