1ab8f9e934382257b7b2e10e94abf5b6f2371434
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-12-23 15:18:55 +0100

    updated test.js

--- src/slashcommands/private/enabled/test.js
+++ src/slashcommands/private/enabled/test.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require('discord.js');//con el paquete ChannelType podemos seleccionar con la opcion .addChannelTypes el tipo de canal a seleccionar
+const { SlashCommandBuilder } = require('discord.js');
+const { ownerid } = require('../../../data/socket.json');
     
     module.exports = {
         data: new SlashCommandBuilder()
@@ -10,6 +11,7 @@ const { SlashCommandBuilder } = require('discord.js');//con el paquete ChannelTy
           .setRequired(true)),
     
     async execute(netherhosting, interaction) {
+      if(interaction.user.id === `${ownerid}`) return interaction.reply({ content:"**:x: | ERROR:** Usted no tiene permiso para usar este comando. Sólo mi dueño puede usarlo.", ephemeral: true}).catch(()=> { null; });
       let user = interaction.options.getUser('miembro');
       interaction.reply("Hecho, ya puse el resultado por consola, revisa consola para ver la salida del comando.")
         console.log(user.tag);
