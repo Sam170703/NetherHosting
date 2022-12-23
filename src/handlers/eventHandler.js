ef1af3b46de0b4ee0066f80dc70955bdb7e09200
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-12-23 23:34:27 +0100

    updated eventHandler.js

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -2,13 +2,13 @@ const {readdirSync} = require('fs');
 const path = require('path');
 
 async function eventHandler(netherhosting){
-    //const dirPath = path.resolve(__dirname, '/home/container/NetherHosting/src/events');
-    const dirPath = path.resolve(__dirname, '../events');
+    const dirPath = path.resolve(__dirname, '/home/container/NetherHosting/src/events');
+    //const dirPath = path.resolve(__dirname, '../events');
     const Files = readdirSync(dirPath)
     .filter((file) => file.endsWith('js'))
     .map(async(file) => {
-        //const event = require(`/home/container/NetherHosting/src/events/${file}`);
-        const event = require(`../events/${file}`);
+        const event = require(`/home/container/NetherHosting/src/events/${file}`);
+        //const event = require(`../events/${file}`);
 
         if (event.once) {
             netherhosting.once(event.name, (...args) => event.execute(netherhosting, ...args));
